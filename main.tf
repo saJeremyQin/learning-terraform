0b2810af715ade61e7e729f61dc29ac5a29b7edb
saJeremyQin <343220868@qq.com>
2025-06-04 20:34:06 +0930

    create alb, run properly

--- main.tf
+++ main.tf
@@ -42,50 +42,50 @@ resource "aws_instance" "mooji" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  # Security Group
+  security_groups = [module.blog_sg.security_group_id]
+
+  listeners = {
+    http = {
+      port = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "web"
+      }
+    }
+  }
+  target_groups = {  
+    web = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.mooji.id
+    }
+  }
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
   name    = "blog"
 
   # vpc_id        = data.aws_vpc.default.id
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id        = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp", "https-443-tcp"] 
   egress_rules  = ["all-all"]
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
-
-# resource "aws_security_group" "blog" {
-#   name        = "blog"
-#   description = "Allow http and https in, allow everything out"
-
-#   vpc_id = data.aws_vpc.default.id
-# }
-
-# resource "aws_security_group_rule" "blog_http_in" {
-#   security_group_id = aws_security_group.blog.id
-#   type        = "ingress"
-#   from_port   = 80
-#   to_port     = 80
-#   protocol    = "tcp"
-#   cidr_blocks = [ "0.0.0.0/0"]
-# }
-
-# resource "aws_security_group_rule" "blog_https_in" {
-#   security_group_id = aws_security_group.blog.id
-#   type        = "ingress"
-#   from_port   = 443
-#   to_port     = 443
-#   protocol    = "tcp"
-#   cidr_blocks = [ "0.0.0.0/0" ]
-# }
-
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   security_group_id = aws_security_group.blog.id
-#   type        = "egress"
-#   from_port   = 0
-#   to_port     = 0
-#   protocol    = "-1"
-#   cidr_blocks = [ "0.0.0.0/0" ]
-# }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,10 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.mooji.arn
 }
+output "alb_dns_name" {
+  value = module.alb.dns_name
+}
+
+# alb_dns_name = "blog-alb-1921363458.ap-southeast-2.elb.amazonaws.com"
+# instance_ami = "ami-0725936c061dd32e4"
+# instance_arn = "arn:aws:ec2:ap-southeast-2:205930647566:instance/i-04a9eabfb9b23f6ef"
